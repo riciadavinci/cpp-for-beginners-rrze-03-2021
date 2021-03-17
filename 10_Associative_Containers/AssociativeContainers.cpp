96bfabac0eeb155d0c89ede7b9e83211a39459de
riciadavinci <rishikesh.nerurkar879@gmail.com>
2021-03-17 17:05:18 +0100

    Worked on exercises for Chapter 10; Finished it

--- 10_Associative_Containers/AssociativeContainers.cpp
+++ 10_Associative_Containers/AssociativeContainers.cpp
@@ -19,7 +19,7 @@
 #include <unordered_map>
 #include <unordered_set>
 
-
+// Here, you can also use std::unordered_map here
 using Container = std::map<int,std::string>;
 
 
@@ -27,7 +27,10 @@ void print( const Container& container )
 {
    // Accessing elements via iterator
    {
-      // TODO
+      for(const auto& element: container){
+         std::cout << "(" << element.first << " : " << element.second << ")\n";
+      }
+      std::cout << "\n";
    }
 }
 
@@ -38,22 +41,54 @@ int main()
 
    // Adding elements to the container
    {
-      // TODO
+      // for both map and unordered_map, you will need to use make_pair
+      // to make a pair of key and value
+      container.insert(std::make_pair(2, "two"));
+      container.insert(std::make_pair(1, "one"));
+      container.insert(std::make_pair(3, "three"));
+
+      // We can also use the sub-script operator to insert/access values in a map
+      // The input to this sub-script is the key
+      // But please use insert() to add elements since it is more conservative
+      // than the sub-script notation
+      container[4] = "four";
+
+      print(container);
    }
 
    // Erasing elements from the container
    {
-      // TODO
+      // Use the member function find instead of using the one 
+      // from "algorithm" header
+      // "erase" takes iterator for the element
+      container.erase(container.find(1));
+      print(container);
    }
 
    // Assignment
    {
-      // TODO
+      Container container2;
+
+      container2 = container;
+
+      if(container != container2){
+         std::cerr << "Invalid assignment!\n";
+      }
    }
 
    // Swapping containers
    {
-      // TODO
+      Container container2;
+      container2.insert(std::make_pair(4, "four"));
+      container2.insert(std::make_pair(5, "five"));
+      container2.insert(std::make_pair(6, "six"));
+
+      std::swap(container, container2);
+      std::cout << "Container: \n";
+      print(container);
+
+      std::cout << "\nContainer2 : \n";
+      print(container2);
    }
 
    return EXIT_SUCCESS;
--- 10_Associative_Containers/Count2.cpp
+++ 10_Associative_Containers/Count2.cpp
@@ -20,7 +20,39 @@
 
 int main()
 {
-   // TODO
+   // using unsigned int for the associated value (count)
+   // since the count cannot be negative
+   std::map<int,unsigned int> occurances;
+   const int MAX_NO_ITER = 10;
+   int input;
+   for(int i = 0; i < MAX_NO_ITER && std::cin; ++i){
+
+
+      
+      if(std::cin >> input){
+         // Clearner Algo:
+         // If the key exists, it will have the given value
+         // But if it is not present, the value will be initialized with
+         // a default value, in this case the "int" is initialized with '0'
+         ++occurances[input];
+
+
+         // My algo and Klaus' first algo:
+         /*
+         const auto it = occurances.find(input);
+         if(it == occurances.end()){
+            occurances.insert(std::make_pair(input, 1));
+         } else {
+            ++(it->second);
+         }
+         */
+      }
+   }
+
+   for( const auto& element: occurances){
+      std::cout << "Key: "<<element.first << ", Value: " << element.second << "\n";
+   }
+   std::cout << "\n";
 
    return EXIT_SUCCESS;
 }
